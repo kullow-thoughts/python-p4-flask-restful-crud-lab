a9665529e36f54ca3c4c18fbc29c0c9e72713c22
Abdulhakimkullow <abdulhakim.kullow@student.moringaschool.com>
2024-07-09 08:37:13 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -47,9 +47,32 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        if plant is None:
+            return make_response(jsonify({"message": "Plant not found"}), 404)
+
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(jsonify(response_dict),200)
+        return response
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return '', 204
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
-
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
